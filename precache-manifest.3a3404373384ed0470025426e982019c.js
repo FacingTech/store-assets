5d47b941f46fa33059b5ea9acbe1534d97c5ab39
ericwdq <wdq1103@163.com>
2020-03-04 17:22:13 +0800

    Add assets

---
+++ precache-manifest.3a3404373384ed0470025426e982019c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29a845a10fc78acf05038d64d60b819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6093b83a35d1e1c7d9",
+    "url": "/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "b292b2d81324c5538470",
+    "url": "/static/css/main.e9157e1b.chunk.css"
+  },
+  {
+    "revision": "ac6093b83a35d1e1c7d9",
+    "url": "/static/js/2.a0db3abe.chunk.js"
+  },
+  {
+    "revision": "4ef09110d66bd9bb43230ec0fb8e3731",
+    "url": "/static/js/2.a0db3abe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b292b2d81324c5538470",
+    "url": "/static/js/main.79f3b4b8.chunk.js"
+  },
+  {
+    "revision": "7b0e7091c5760ff319bc",
+    "url": "/static/js/runtime-main.d3518652.js"
+  },
+  {
+    "revision": "60a8874b9dad44ac35b22643daa14b25",
+    "url": "/static/media/logo.60a8874b.png"
+  },
+  {
+    "revision": "c531930855aa352961cf62f5b113c5cc",
+    "url": "/static/media/profile.c5319308.png"
+  },
+  {
+    "revision": "acb65a9e617857adf2b957f44156b99e",
+    "url": "/static/media/storeFront.acb65a9e.jpg"
+  }
+]);
